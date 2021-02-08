962c2f921f8ce1efd9bba4e76414030e973babaa
Niccolo Delfino <delfino.nicco@libero.it>
2021-02-08 13:33:01 +0100

    Exercise ok

--- js/script.js
+++ js/script.js
@@ -3,10 +3,16 @@
 var app = new Vue({
   el: `#app`,
   data: {
-    mail : 0,
+    mail : [],
   },
   mounted(){
-
+    for(let i = 0; i < 10; i++){
+      axios
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((result) => {
+        this.mail.push(result.data.response);
+      });
+    }
   },
   methods: {
 
